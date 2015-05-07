1f2ed065c79bfb42b7d17e1d154096fac540c99c
Nathan Johnstone <locotewz@gmail.com>
2015-05-07 20:49:09 +1200

    add unit test project add nunit and fluentAssertions

---
+++ Projects/CsProjArrange/CsProjArrange.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace CsProjArrange.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
