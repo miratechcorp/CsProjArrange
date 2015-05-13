f39df6769d4289914661ce07f2756f9f83119e5c
Nathan Johnstone <locotewz@gmail.com>
2015-05-13 21:38:20 +1200

    modify test data files to be embedded resources and add helper static methods to extract them to streams or XDocuments refactor unit tests to use embedded resources

---
+++ Projects/CsProjArrange/CsProjArrange.Tests/Helpers/EmbeddedResourceHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CsProjArrange.Tests.Helpers
+{
+    public class EmbeddedResourceHelper
+    {
+        public static Stream ExtractManifestResourceToDisk(string relativeManifestUri)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+
+            var uri = String.Format("{0}.{1}", assembly.GetName().Name, relativeManifestUri);
+
+            return assembly.GetManifestResourceStream(uri);
+        }
+
+        public static XDocument ExtractManifestResourceAsXDocument(string relativeManifestUri)
+        {
+            XDocument doc;
+            using (var stream = ExtractManifestResourceToDisk(relativeManifestUri))
+            {
+                doc = XDocument.Load(stream);
+            }
+            return doc;
+        }
+
+    }
+}
--- Projects/CsProjArrange/CsProjArrange.Tests/UnitTest1.cs
+++ Projects/CsProjArrange/CsProjArrange.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Xml.Linq;
+using CsProjArrange.Tests.Helpers;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
@@ -17,39 +19,43 @@ namespace CsProjArrange.Tests
         [Test]
         public void GetSomeData()
         {
+            XDocument inputDocument =
+                EmbeddedResourceHelper.ExtractManifestResourceAsXDocument("TestData.Input.CsProjArrangeInput.csproj");
             var target = new CsProjArrange();
 
-            target.Arrange(@".\TestData\CsProjArrangeInput.csproj", @"CsProjArrangeExpectedCombineRootElements.csproj", null, null, CsProjArrange.ArrangeOptions.CombineRootElements);
+            target.Arrange(inputDocument, null, null, CsProjArrange.ArrangeOptions.CombineRootElements);
 
-
+            inputDocument.Save(@"CsProjArrangeExpectedCombineRootElements.csproj");
         }
 
         [Test]
         public void Arrange_when_defaults_used_should_return_expected_csproj()
         {
+            XDocument inputDocument =
+                EmbeddedResourceHelper.ExtractManifestResourceAsXDocument("TestData.Input.CsProjArrangeInput.csproj");
             var target = CreateTestTarget();
 
             // Act
-            target.Arrange(@".\TestData\CsProjArrangeInput.csproj", "CsProjActualOutput.csproj", null, null, CsProjArrange.ArrangeOptions.None);
+            target.Arrange(inputDocument, null, null, CsProjArrange.ArrangeOptions.None);
 
             // Assert
-            string[] actualOutput = File.ReadAllLines(@"CsProjActualOutput.csproj");
-            string[] expectedOutput = File.ReadAllLines(@".\TestData\Expected\CsProjArrangeExpectedDefault.csproj");
-            actualOutput.Should().Equal(expectedOutput);
+            XDocument expectedDocument = EmbeddedResourceHelper.ExtractManifestResourceAsXDocument("TestData.Expected.CsProjArrangeExpectedDefault.csproj");
+            inputDocument.ToString().Should().BeEquivalentTo(expectedDocument.ToString());
         }
 
         [Test]
         public void Arrange_when_combineRootElements_used_should_return_expected_csproj()
         {
+            XDocument inputDocument =
+                EmbeddedResourceHelper.ExtractManifestResourceAsXDocument("TestData.Input.CsProjArrangeInput.csproj");
             var target = CreateTestTarget();
 
             // Act
-            target.Arrange(@".\TestData\CsProjArrangeInput.csproj", "CsProjActualOutput.csproj", null, null, CsProjArrange.ArrangeOptions.CombineRootElements);
+            target.Arrange(inputDocument, null, null, CsProjArrange.ArrangeOptions.CombineRootElements);
 
             // Assert
-            string[] actualOutput = File.ReadAllLines(@"CsProjActualOutput.csproj");
-            string[] expectedOutput = File.ReadAllLines(@".\TestData\Expected\CsProjArrangeExpectedCombineRootElements.csproj");
-            actualOutput.Should().Equal(expectedOutput);
+            XDocument expectedDocument = EmbeddedResourceHelper.ExtractManifestResourceAsXDocument("TestData.Expected.CsProjArrangeExpectedCombineRootElements.csproj");
+            inputDocument.ToString().Should().BeEquivalentTo(expectedDocument.ToString());
         }
 
     }
