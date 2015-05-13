49629f0beffda33624bcce133e8de9387d434591
Nathan Johnstone <locotewz@gmail.com>
2015-05-13 21:16:05 +1200

    write first test using previously created test data to assert current functionality

--- Projects/CsProjArrange/CsProjArrange.Tests/UnitTest1.cs
+++ Projects/CsProjArrange/CsProjArrange.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 
@@ -7,6 +9,11 @@ namespace CsProjArrange.Tests
     [TestFixture]
     public class UnitTest1
     {
+        private static CsProjArrange CreateTestTarget()
+        {
+            return new CsProjArrange();
+        }
+
         [Test]
         public void GetSomeData()
         {
@@ -16,5 +23,20 @@ namespace CsProjArrange.Tests
 
 
         }
+
+        [Test]
+        public void Arrange_when_defaults_used_should_return_expected_csproj()
+        {
+            var target = CreateTestTarget();
+
+            // Act
+            target.Arrange(@".\TestData\CsProjArrangeInput.csproj", "CsProjActualOutput.csproj", null, null, CsProjArrange.ArrangeOptions.None);
+
+            // Assert
+            string[] actualOutput = File.ReadAllLines(@"CsProjActualOutput.csproj");
+            string[] expectedOutput = File.ReadAllLines(@".\TestData\Expected\CsProjArrangeExpectedDefault.csproj");
+            actualOutput.Should().Equal(expectedOutput);
+        }
+
     }
 }
